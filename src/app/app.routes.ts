4ae79851238848ac94609050ff7e1a7938848d03
oshanAmarasuriya <oshansandeep630@gmail.com>
2024-06-14 19:21:04 +0530

    completed routing for pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { VotingComponent } from './voting/voting.component';
+import { MapViewComponent } from './map-view/map-view.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent}
+    { path: 'home', component: HomeComponent},
+    { path: 'voting', component: VotingComponent},
+    { path: 'map', component: MapViewComponent}
 ];
---
+++ src/app/voting/voting.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-voting',
+  standalone: true,
+  imports: [],
+  templateUrl: './voting.component.html',
+  styleUrl: './voting.component.css'
+})
+export class VotingComponent {
+
+}
